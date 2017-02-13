5d307a3e49406c39b7028df1b356cca720b7b4f2
S. Hutchins <sdhutchins@users.noreply.github.com>
2017-02-13 02:07:14 -0600

    Update BBReferenceScrape.py

--- BBReferenceScrape.py
+++ BBReferenceScrape.py
@@ -1,12 +1,32 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import os
+from collections import OrderedDict
 
+# Quick directories work
+path = 'NBA_Monthly_Schedules'
+if os.path.exists(path):
+    pass
+else:
+    os.mkdir(path)
+
+# Find a base form of the url to use
 baseurl = 'http://www.basketball-reference.com/leagues/NBA_2017_'
-months = {'oct': 'games.html', 'nov': 'games-november.html', 'dec': 'games-december.html',
-          'jan': 'games-january.html', 'feb': 'games-february.html',
-          'mar': 'games-march.html', 'apr': 'games-april.html'}
 
+# Create a dictory of keys and values to use with a for loop to create the url
+# that you want to get data from
+months = OrderedDict([('oct', 'games.html'), ('nov', 'games-november.html'), 
+                      ('dec', 'games-december.html'), ('jan', 'games-january.html'), 
+                      ('feb', 'games-february.html'), ('mar', 'games-march.html'), 
+                      ('apr', 'games-april.html')])
+
+os.chdir(path)
+
+# Use the xlsxwriter to create an excel file with each sheet being a month
+writer = pd.ExcelWriter('NBA_Schedule_16-17.xlsx', engine='xlsxwriter')
+
+# Create a for loop that iterates over the months dictionary and gets schedules
 for key, value in months.items():
     url = baseurl + value    
     response = requests.get(url)
@@ -32,5 +52,15 @@ for key, value in months.items():
     	r = row[:6]
     	list_of_rows2.append(r)
         
+    
+    frames = {}
+    monthslist = list(months.keys())
+    m = key
     df = pd.DataFrame(data=list_of_rows2, dtype=str)
     df.to_csv('nba_' + key + '_schedule.csv', header=None, index=False)
+    frames[m] = df
+    
+    for sheet, frame in frames.items():
+        frame.to_excel(writer, sheet_name = sheet, header=None, index=False)
+
+writer.save()
